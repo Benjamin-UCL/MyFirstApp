b904f8e895d1c9b1f48adc882d2bf38bec34ba53
Benjamin Leonhardt <bjle71347@edu.ucl.dk>
2025-02-20 10:49:03 +0100

    projekt start

---
+++ Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Benjamin_CarApp
+{
+
+    public static class menu
+    {
+        public static char prompt()
+        {
+            Console.WriteLine("SELECT ACTION");
+            Console.WriteLine("(R)egister car");
+            Console.WriteLine("(P)rint car");
+            Console.WriteLine("(V)iew all cars");
+            Console.WriteLine("(D)rive");
+
+            return Char.ToUpper(Console.ReadLine()[0]);
+        }
+
+        public static void handel(char code)
+        {
+            switch (code)
+            {
+                case 'R':
+
+                    Console.WriteLine("Register car");
+                    break;
+
+                case 'P':
+
+                    Console.WriteLine("Print car");
+                    break;
+
+                case 'V':
+
+                    Console.WriteLine("View all cars");
+                    break;
+
+                case 'D':
+
+                    Console.WriteLine("Drive car");
+                    break;
+
+                default:
+                    Console.WriteLine("Try again");
+                    prompt();
+                    break;
+            }
+        }
+    } 
+
+    public static class response
+    {
+        public static void print(string[] list)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class Car
+    {
+        private string brand;
+        private string model;
+        private int year = 0000;
+        private bool acceptedYear = false;
+        private char geartype = '0';
+        private bool acceptedgeartype = false;
+        private char fule = '0';
+        private bool acceptedFuletype = false;
+        private float miles = 0;
+        private float milage = 0;
+
+        public Car()
+        {
+            Console.Write("Brand: ");
+            brand = Console.ReadLine();
+            Console.Write("Model: ");
+            model = Console.ReadLine();
+
+            // year validation
+            while (!acceptedYear)
+            {
+                int err = 0;
+
+                Console.Write("Year: ");
+                year = int.Parse(Console.ReadLine());
+
+                if (year == null) { err++; }
+                if (year > int.Parse(DateTime.Now.ToString("yyyy"))) { err++; }
+                if (year < 999) { err++; }
+
+                if (err == 0) { acceptedYear = true; }
+            }
+
+            //geartype validation
+            while (!acceptedgeartype)
+            {
+                Console.Write("type ((A)utomatic/(M)anual): ");
+                geartype = char.ToUpper(Console.ReadLine()[0]);
+
+                if (geartype == 'M' || geartype == 'A') { acceptedgeartype = true; }
+            }
+
+            while (!acceptedFuletype)
+            {
+                Console.Write("brandstof type ((B)enzin/(D)isel): ");
+                fule = char.ToUpper(Console.ReadLine()[0]);
+
+                if (fule == 'D' || fule == 'B') { acceptedFuletype = true; }
+            }
+
+            Console.Write("Km p/l: ");
+            milage = float.Parse(Console.ReadLine());
+            Console.Write("kilometertæller: ");
+            miles = float.Parse(Console.ReadLine());
+        }
+
+        public void print()
+        {
+            string lineBrand = String.Format("{0,-15}{1, 15}", "Brand:", brand);
+            string lineModel = String.Format("{0,-15}{1, 15}", "Model:", model);
+            string lineGear = String.Format("{0,-15}{1, 15}", "type:", geartype);
+            string lineYear = String.Format("{0,-15}{1, 15}", "År:", year);
+            string lineFuletype = String.Format("{0,-15}{1, 15}", "Brændstof:", fule);
+            string lineMilage = String.Format("{0,-15}{1, 15}", "km/l:", milage);
+            string lineMiles = String.Format("{0,-15}{1, 15}", "km:", miles);
+
+            string[] res = { "___________OVERVIEW___________", lineBrand, lineModel, lineGear, lineYear, lineFuletype, lineMilage, lineMiles };
+
+            response.print(res);
+        }
+
+        public void drive() { 
+        
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            // spørg køretur
+            Console.Write("Ønskede distance (km): ");
+            float trip = float.Parse(Console.ReadLine());
+            miles = miles + trip;
+
+            // pris
+            float benzinPris = 12.7F;
+            float diselPris = 10.57F;
+
+            float brandstofspris = benzinPris;
+            if (fule != "B"[0]) { brandstofspris = diselPris; }
+
+            float liter = trip / milage;
+            float pris = liter * benzinPris;
+
+
+
+            // Print kvitering
+            string details = trip + " km af " + benzinPris + "kr/l";
+            string subtotal = (pris/trip) + " pr. km";
+            string lineTripTotal = String.Format("{0,-15}{1, 15}", "Total kr:", pris);
+
+
+            string[] res = { "__________KVITERING__________", details, subtotal, lineTripTotal };
+
+            response.print(res);
+
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car myCar = new Car();
+
+            //myCar.print();
+            //myCar.drive();
+            //myCar.print();
+
+            menu.handel(menu.prompt());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
